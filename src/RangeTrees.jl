703ffcba6c4d3024c3e39ee3c548e53608a63f1a
Iulia Dumitru <iuliadmtru@gmail.com>
2025-03-13 20:12:37 +0200

    Add insert and a few constructors

--- src/RangeTrees.jl
+++ src/RangeTrees.jl
@@ -3,20 +3,35 @@ module RangeTrees
 # Existing similar package: https://github.com/BioJulia/IntervalTrees.jl. Not maintained, outdated.
 # Might be useful: https://github.com/JuliaMath/IntervalSets.jl
 
+export RangeNode
+
 using AbstractTrees
 
 #=
     Definition and constructors.
 =#
 
-struct RangeNode{T} <: AbstractNode{T}
+mutable struct RangeNode{T} <: AbstractNode{T}
     interval::UnitRange # Should keep `low` and `high` instead?
-    left_child::Union{RangeNode, Nothing}
-    right_child::Union{RangeNode, Nothing}
+    left::Union{RangeNode, Nothing}
+    right::Union{RangeNode, Nothing}
     parent::Union{RangeNode, Nothing}
     max::Int
     data::T # Extra data. In our case, the corresponding `GreenNode`.
 end
+
+function RangeNode(interval::UnitRange, parent::RangeNode, data::T) where T
+    return RangeNode(
+        interval,
+        nothing,
+        nothing,
+        parent,
+        interval.stop,
+        data
+    )
+end
+RangeNode(interval::UnitRange, parent::RangeNode) = RangeNode(interval, parent, nothing)
+
 function RangeNode(interval::UnitRange, data::T) where T
     return RangeNode(
         interval,
@@ -27,12 +42,13 @@ function RangeNode(interval::UnitRange, data::T) where T
         data
     )
 end
+RangeNode(interval::UnitRange) = RangeNode(interval, nothing)
 
 #=
     Getters and setters.
 =#
 
-AbstractTrees.children(node::RangeNode) = [node.left_child, node.right_child]
+AbstractTrees.children(node::RangeNode) = [node.left, node.right]
 
 AbstractTrees.nodevalue(node::RangeNode) = node.interval
 
@@ -47,12 +63,37 @@ nodedata(node::RangeNode) = node.data
 
 function Base.insert!(root::Union{RangeNode, Nothing}, interval::UnitRange, data::T) where T
     if isnothing(root)
+        @info "Here"
         return RangeNode(interval, data)
     end
 
+    left = root.left
+    right = root.right
     if interval.start < root.interval.start
-        insert!(root.left_child, interval, data)
+        if isnothing(left)
+            root.left = RangeNode(interval, root, data)
+        elseif left.interval.start == interval.start && left.interval.stop == interval.stop
+            @info "Interval $interval already exists."
+            return root
+        else
+            insert!(root.left, interval, data)
+        end
+    else
+        if isnothing(right)
+            root.right = RangeNode(interval, root, data)
+        elseif right.interval.start == interval.start && right.interval.stop == interval.stop
+            @info "Interval $interval already exists."
+            return root
+        else
+            insert!(root.right, interval, data)
+        end
     end
+
+    if root.max < interval.stop
+        root.max = interval.stop
+    end
+
+    return root
 end
 
 end # module RangeTrees
